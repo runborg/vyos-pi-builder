877bde3d367d2bd3f09367f710f6520163ff5c7a
Runar Borge <runar@borge.nu>
2021-03-27 07:31:44 +0100

    Remove packages now present in the official APT repository
    A new official CI pipeline is now running for compiling most packages used
    as for now only kernel, accel-ppp and vyos-1x is needed to be custom buildt

--- build-packages.sh
+++ build-packages.sh
@@ -63,26 +63,15 @@ eval $(opam env --root=/opt/opam --set-root)
 
 # Build vyos repositories:
 # Disabled repos: conntrack-tools
-for x in vyos-1x vyos-utils vyos-world vyatta-conntrack mdns-repeater hvinfo ipaddrcheck libvyosconfig libnss-mapuser libpam-radius-auth vyos-strongswan libnetfilter-conntrack vyatta-cfg udp-broadcast-relay vyatta-bash vyatta-wanloadbalance vyos-opennhrp vyos-nhrp vyatta-cfg-vpn vyatta-op-vpn; do
+for x in vyos-1x; do
     cd $BUILDDIR
     echo "Checking for $x"
     FILECHECK=$x
-    if [[ "$x" == "vyos-strongswan" ]]; then
-        FILECHECK="strongswan"
-    fi
     if ! ls *$FILECHECK*.deb; then
 	if [ ! -d $x ]; then
 	    echo "$x not found, fetching"
 	    git clone git://github.com/vyos/$x $x
 	fi
-	if [[ "$x" == "vyos-utils" ]]; then
-            echo "Installing deps for vyos-utils"
-	    opam install containers
-	fi
-	if [[ "$x" == "conntrack-tools" ]]; then
-            echo "Installing deps for conntrack-tools"
-            apt install -y dh-systemd libnetfilter-conntrack-dev
-        fi
 	if [[ "$x" == "vyos-1x" ]]; then
 	    sudo patch -p 1 -u -d $BUILDDIR/$x < $CWD/vyos-1x_disable_xdp_patch.patch
 	fi
@@ -92,20 +81,6 @@ for x in vyos-1x vyos-utils vyos-world vyatta-conntrack mdns-repeater hvinfo ipa
     fi
 done
 
-
-# Build FRR
-
-cd $BUILDDIR/vyos-build/packages/frr
-FRR_VERSION=$(sed -ne "s/.*scmCommit.: '\(.*\)',/\1/p" Jenkinsfile)
-FRR_REPO=$(sed -ne "s/.*scmUrl.: '\(.*\)',/\1/p" Jenkinsfile)
-if ! ls frr*.deb; then
-    if [ ! -d frr ]; then
-        echo "Fetching FRR"
-        git clone ${FRR_REPO} -b ${FRR_VERSION}
-    fi
-    echo "Building FRR"
-    ./build-frr.sh
-fi
 
 # Linux kernel
 cd $BUILDDIR/vyos-build/packages/linux-kernel
@@ -143,73 +118,4 @@ if ! ls vyos-linux-firmware*.deb; then
         git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git linux-firmware
     fi
     bash -x -e ./build-linux-firmware.sh
-fi
-
-#IPRoute2
-cd $BUILDDIR/vyos-build/packages/iproute2/
-if ! ls iproute2*.deb; then
-    if [ ! -d iproute2 ]; then
-        echo "Fetching linux-firmware"
-        git clone https://salsa.debian.org/debian/iproute2 -b debian/5.10.0-3 iproute2
-    fi
-      cd $BUILDDIR/vyos-build/packages/iproute2/iproute2
-      dpkg-buildpackage -uc -us -tc -b -d
-fi
-
-# Netfilter
-cd $BUILDDIR/vyos-build/packages/netfilter
-if ! ls *libnftnl*.deb; then
-    if [ ! -d pkg-libnftnl ]; then
-        echo "Fetching pkg-netfilter"
-          git clone https://salsa.debian.org/pkg-netfilter-team/pkg-libnftnl.git pkg-libnftnl
-    fi
-    cd $BUILDDIR/vyos-build/packages/netfilter/pkg-libnftnl
-    dpkg-buildpackage -uc -us -tc -b -d
-fi
-
-# Netfilter - nftables
-cd $BUILDDIR/vyos-build/packages/netfilter
-if ! ls *nftables*.deb; then
-    if [ ! -d pkg-nftables ]; then
-        echo "Fetching nftables"
-          git clone https://salsa.debian.org/pkg-netfilter-team/pkg-nftables.git -b 'debian/0.9.6-1' pkg-nftables
-    fi
-    cd $BUILDDIR/vyos-build/packages/netfilter/pkg-nftables
-    dpkg -i ../libnftnl*.deb 
-    sed -i "s/debhelper-compat.*/debhelper-compat (= 12),/" debian/control 
-    dpkg-buildpackage -uc -us -tc -b -d
-fi
-
-# libnetfilter-conntrack
-cd $BUILDDIR/vyos-build/packages/netfilter
-if ! ls *libnetfilter-conntrack*.deb; then
-    if [ ! -d pkg-libnetfilter-conntrack ]; then
-        echo "Fetching pkg-libnetfilter-conntrack"
-          git clone https://salsa.debian.org/pkg-netfilter-team/pkg-libnetfilter-conntrack.git -b 'debian/1.0.8-1' pkg-libnetfilter-conntrack
-    fi
-      cd $BUILDDIR/vyos-build/packages/netfilter/pkg-libnetfilter-conntrack
-      dpkg-buildpackage -uc -us -tc -b -d
-fi
-
-# conntrack-tools
-cd $BUILDDIR/vyos-build/packages/netfilter
-if ! ls *conntrack*.deb; then
-    if [ ! -d pkg-conntrack-tools ]; then
-        echo "Fetching pkg-conntrack-tools"
-          git clone https://salsa.debian.org/pkg-netfilter-team/pkg-conntrack-tools.git -b 'debian/1%1.4.6-1' pkg-conntrack-tools
-    fi
-      cd $BUILDDIR/vyos-build/packages/netfilter/pkg-conntrack-tools
-      sudo dpkg -i ../libnetfilter*.deb
-      dpkg-buildpackage -uc -us -tc -b -d
-fi
-
-# wide-dhcpv6
-cd $BUILDDIR/vyos-build/packages/wide-dhcpv6
-if ! ls *.deb; then
-    if [ ! -d wide-dhcpv6 ]; then
-        echo "Fetching wide-dhcpv6"
-          git clone https://salsa.debian.org/debian/wide-dhcpv6 -b 'debian/20080615-23' wide-dhcpv6
-    fi
-    cd $BUILDDIR/vyos-build/packages/wide-dhcpv6 
-    bash -x -e ./build-wide.sh
 fi
