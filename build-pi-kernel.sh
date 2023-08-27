77541ad78c49f142025aebba1309f2aa6e4f856c
Jalen Lin <jalen@driversp.com>
2023-08-27 08:31:39 +0800

    Add patch for rpi 6.1.y kernel to fullfill vyos kernel requirements. Remove unnecessary patch for kernel. Update build-pi-kernel.sh to make new pi kernel. (For kernel 6.1.y.)

--- build-pi-kernel.sh
+++ build-pi-kernel.sh
@@ -6,21 +6,21 @@ ROOTDIR=$(pwd)
 rm -rf vyos-build
 git clone http://github.com/vyos/vyos-build vyos-build
 
-# Patch to build-linux-firmware.sh
-patch -t -u vyos-build/packages/linux-kernel/build-linux-firmware.sh < patches/0000_build-linux-firmware.sh.patch
-
-# Patch to build-kernel.sh
-patch -t -u vyos-build/packages/linux-kernel/build-kernel.sh < patches/0001_build-kernel.sh.patch
 
 cd vyos-build/packages/linux-kernel/
 
 echo "Build kernel for pi"
 git clone https://github.com/raspberrypi/linux
+git checkout -b rpi-6.1.y
+cp linux/arch/arm64/configs/bcm2711_defconfig arch/arm64/configs/vyos_defconfig
+patch -t -u arch/arm64/configs/vyos_defconfig < ${ROOTDIR}/patches/0001_bcm2711_defconfig.patch
 ./build-kernel.sh
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 ./build-linux-firmware.sh
 git clone https://github.com/accel-ppp/accel-ppp.git
 ./build-accel-ppp.sh
+git clone --depth=1 https://github.com/OpenVPN/ovpn-dco -b v0.2.20230426
+./build-openvpn-dco.sh
 
 cd ${ROOTDIR}
 mkdir -p build
